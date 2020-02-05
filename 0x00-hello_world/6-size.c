7f834ab0ac58e44ae9e6808021fa6dcd0a96b5e9
danielj32 <danieljuanito21@hotmail.com>
2020-02-05 21:43:37 +0000

    added file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
 
 int main(void)
 {
-	int main(void)
-		
 		char j;
-	int j;
-
+		int k;
+		long l;
+		long long int m;
+		float r;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(r));
 	return (0);
 }
