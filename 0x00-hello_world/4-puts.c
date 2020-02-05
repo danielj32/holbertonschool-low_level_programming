3f8dc5587975d1a0613f66356ecc0eb6b473515d
danielj32 <danieljuanito21@hotmail.com>
2020-02-05 19:29:48 +0000

    added file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ts1[50];
+strcpy(ts1, "\"Programming is like building a multilingual puzzle\n");
+puts(ts1);
+return (0);
+}
