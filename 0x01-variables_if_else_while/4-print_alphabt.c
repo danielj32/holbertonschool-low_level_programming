0f2bf145907e28357a1a88785ea273058b201ea0
danielj32 <danieljuanito21@hotmail.com>
2020-02-06 23:28:11 +0000

    added file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - lowercase
+ *
+ * Return: the value 0
+ */
+
+int main(void)
+{
+char g;
+g = 'a';
+for (; g <= 'z'; g++)
+{
+if (g == 'e' || g == 'q')
+{
+continue;
+}	
+putchar(g);	
+		
+}
+putchar('\n');
+return (0);
+}
