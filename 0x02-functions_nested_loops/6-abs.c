1857e4857ac48f8f4e8fe0aaa1323158380a009e
danielj32 <danieljuanito21@hotmail.com>
2020-02-11 17:18:56 +0000

    added files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+   *_abs - print the lowercase or uppercase.
+    *@d: integer
+     *Return: d or the 0
+      */
+
+int _abs(int d)
+{
+
+		if (d < 0)
+		{
+			return (-d);
+		}
+	return (d);
+}
