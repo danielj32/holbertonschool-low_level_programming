5d3cf0ea934d5f56fe7120b8e8193ee037288bc3
danielj32 <danieljuanito21@hotmail.com>
2020-02-11 22:14:09 +0000

    fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,22 @@ void jack_bauer(void)
 
 	for (tm1 = '0'; tm1 <= '2'; tm1++)
 	{
-		for (tm2 = '0'; tm2 <= '3'; tm2++)
+		for (tm2 = '0'; tm2 <= '9'; tm2++)
 		{
 
 			for (tm3 = '0'; tm3 <= '5'; tm3++)
 			{
 				for (tm4 = '0'; tm4 <= '9'; tm4++)
 				{
-					_putchar(tm1);
-					_putchar(tm2);
-					_putchar(':');
-					_putchar(tm3);
-					_putchar(tm4);
-					_putchar('\n');
+					if ((tm1 != '2' && tm2 <= '9') || (tm1 == '2' && tm2 <= '3'))
+					{
+						_putchar(tm1);
+						_putchar(tm2);
+						_putchar(':');
+						_putchar(tm3);
+						_putchar(tm4);
+						_putchar('\n');
+					}
 
 				}
 			}
