7bace18d521deb68a43f0636fb6f529539c7f05f
danielj32 <danieljuanito21@hotmail.com>
2020-02-11 04:10:05 +0000

    added files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - print the alphabet.
+ *@void: null
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	char ab;
+
+	while (j <= 10)
+		{
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+		{
+
+		_putchar(ab);
+
+		}
+	putchar('\n');
+	j++;
+		}
+
+}
