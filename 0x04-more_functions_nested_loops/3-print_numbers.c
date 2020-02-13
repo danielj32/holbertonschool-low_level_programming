0e8a47c40cfd536879a9e27e8a8f36c43d0ebb5e
danielj32 <danieljuanito21@hotmail.com>
2020-02-13 16:40:02 +0000

    [CREATE] - print number to 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*print_numbers - prints the numbers, from 0 to 9.
+*@void: null.
+*/
+
+void print_numbers(void)
+{
+int al;
+	for (al = 48; al <= 57; al++)
+
+		_putchar(al);
+	_putchar('\n');
+}
