f48db4ebb39238945b8a4edab7a5b735869a7d76
danielj32 <danieljuanito21@hotmail.com>
2020-02-13 16:19:47 +0000

    [CREATE] - new function multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*mul - multiplies two integers.
+*@a: integer
+*@b: second integer.
+*Return: the result of the function.
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
+
