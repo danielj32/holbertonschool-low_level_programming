8962cf010f09204a1759f8ab4a25d439b1ff1e77
danielj32 <danieljuanito21@hotmail.com>
2020-02-18 23:05:33 +0000

    [CREATE] - prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+  *puts2 - prints every other character of a string.
+  *@str: string
+  *Return: ct
+  */
+int _strlen(char *str);
+void puts2(char *str);
+
+void puts2(char *str)
+{
+	int ct;
+	 char s[32];
+
+	for (ct = 0; ct < _strlen(str); ct += 2)
+	{
+		_putchar(s[20]);
+		_putchar(str[ct]);
+		s[ct / 2] = str[ct];
+	}
+	_putchar(10);
+
+}
+/**
+ *_strlen - length of a string.
+ *@str: pointer.
+ *
+ *Return: ct.
+ */
+
+
+int _strlen(char *str)
+{
+	int ct = 0;
+
+	while (*str != '\0')
+	{
+		ct++;
+		str++;
+	}
+	return (ct);
+}
+
