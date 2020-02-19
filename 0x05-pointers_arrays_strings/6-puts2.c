1b57f9399dbd9feba1e195676bb2d8a231bdbf39
danielj32 <danieljuanito21@hotmail.com>
2020-02-19 01:35:35 +0000

    [UPDATE] - fixed delete if

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,21 +5,14 @@
   *Return: ct
   */
 int _strlen(char *str);
-void puts2(char *str);
 
 void puts2(char *str)
 {
 	int ct;
-	char s[64] = " ";
 
 		for (ct = 0; ct < _strlen(str); ct += 2)
 		{
-			if (ct % 2 == '\0')
-			{
-				_putchar(s[10]);
-				_putchar(str[ct]);
-				s[ct / 2] = str[ct];
-			}
+			_putchar(str[ct]);
 		}
 		_putchar(10);
 
