2852c9081c69bf822f6b55678cc33507ef986c05
danielj32 <danieljuanito21@hotmail.com>
2020-02-20 21:31:13 +0000

    [CREATE] - compares two strings in ascii

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  *_strcmp - compares two strings
+  *@s1: pointer char 1
+  *@s2: pointer char 2
+  *Return: the values comparative
+  */
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
