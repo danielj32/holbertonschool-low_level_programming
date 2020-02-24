44c28bbfd3f3899fc9a849b9d19f2f0c2a0ecbdd
danielj32 <danieljuanito21@hotmail.com>
2020-02-24 21:41:43 +0000

    [CREATE] - added new function prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ ** _strspn - length of a prefix substring.
+ *@s: pointer char
+ *@accept: 2 char.
+ *Return: a value
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	while (*s != '\0'  &&  _strchr(accept, *s++))
+		i++;
+	return (i);
+}
+
+/**
+ *_strchr - locates a character in a string.
+ *@s: pointer of the string.
+ *@c: char datatype.
+ *Return: null first occurrence of c or NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		if (!*s++)
+			return (0);
+	return (s);
+}
