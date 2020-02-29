efc1f4cd0f555abfca79e002ac2740010a9ec829
danielj32 <danieljuanito21@hotmail.com>
2020-02-29 04:12:02 +0000

    [CREATE] - prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - function principal
+ *@ac: number arguments passed into it
+ *@av: null
+ *Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	(void)av;
+
+	printf("%d\n", ac - 1);
+	return (0);
+}
