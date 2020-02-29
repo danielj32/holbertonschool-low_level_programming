4b2644c27bf76cc1d8581a17ca7df21b8b133a20
danielj32 <danieljuanito21@hotmail.com>
2020-02-29 03:41:09 +0000

    [CREATE] - added argc and argv function print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - principal funtion
+ *@argc: count the arguments
+ *@argv: array of pointers
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
