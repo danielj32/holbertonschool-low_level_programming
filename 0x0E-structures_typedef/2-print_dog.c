93d29aba0af79825fc1b8871a6bf9f31891895fc
danielj32 <danieljuanito21@hotmail.com>
2020-03-09 22:30:59 +0000

    [CREATE] - added files

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+/**
+ *print_dog - print struct
+ *struct dog - struct
+ *@d: pointer to structure.
+ *Return: nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+
+}
