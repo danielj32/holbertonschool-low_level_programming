7e3153752372e5a60c3f40e0476f9b584afd5bd5
danielj32 <danieljuanito21@hotmail.com>
2020-03-09 18:27:18 +0000

    [CREATE] - added files and new structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains examples of how to use structures in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - name of structure
+ *@name: var name
+ *@age: float
+ *@owner: char
+ *my_dog - alias
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+my_dog;
+
+#endif
