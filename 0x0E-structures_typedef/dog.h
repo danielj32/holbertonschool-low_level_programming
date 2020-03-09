9b7442fb0303f884ea408590ed285937e76f59a8
danielj32 <danieljuanito21@hotmail.com>
2020-03-09 22:55:04 +0000

    [CREATE] -added file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  *struct dog - name of structure
@@ -11,15 +9,16 @@
  *my_dog - alias
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-my_dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
