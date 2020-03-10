3b2d976bffc611737b9873198c13274aaefa710a
danielj32 <danieljuanito21@hotmail.com>
2020-03-10 00:16:26 +0000

    [CREATE] - added file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *struct dog - name of structure
  *@name: var name
@@ -20,5 +21,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strdup(char *str);
 
 #endif
