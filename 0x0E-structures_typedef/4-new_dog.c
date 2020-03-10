d0cdb9940632721e5d8430a9f4f5909b9bcdb889
danielj32 <danieljuanito21@hotmail.com>
2020-03-10 00:49:59 +0000

    [CREATE] - added file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,9 +28,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	new_dog->owner = _strdup(owner);
 	if (new_dog->owner == NULL)
+		{
 		free(new_dog->name);
 		free(new_dog);
-
+		}
 	return (new_dog);
 }
 
