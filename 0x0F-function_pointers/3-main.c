0f4eccd67470c10b2e0c52b323df838e8dfd1182
danielj32 <danieljuanito21@hotmail.com>
2020-03-12 02:48:51 +0000

    [CREATE] - added file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+
+int main(int ac, char *av[])
+{
+
+	char *str = NULL;
+	int n1 = 0;
+	int n2 = 0;
+	int res = 0;
+
+	if (ac != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	str = av[2];
+	n1 = atoi(av[1]);
+	n2 = atoi(av[3]);
+		  res = get_op_func(str)(n1, n2);
+		  if (res == 0)
+			  {
+				  printf("Error\n");
+				  exit(99);
+			  }
+		  printf("%d\n", res);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+
+int op_add(int b, int c)
+{
+	return (b + c);
+}
+int op_sub(int b, int c)
+{
+	return (b - c);
+}
+int op_mul(int b, int c)
+{
+	return(b * c);
+}
+int op_div(int b, int c)
+{
+	return (b / c);
+}
+int op_mod(int b,int c)
+{
+	return(b % c);
+}
