361c550d46697f8ef62294a44256382a3f3889c8
danielj32 <danieljuanito21@hotmail.com>
2020-03-12 03:13:30 +0000

    [fix] - added file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,22 +1,53 @@
 #include "3-calc.h"
+/**
+ *op_add - ss
+ *@b: int
+ *@c: int
+ *Return: 0
+ */
 
 int op_add(int b, int c)
 {
 	return (b + c);
 }
+/**
+ *op_sub - fd
+*@b: int
+*@c: int
+ *Return: 0
+ */
 int op_sub(int b, int c)
 {
 	return (b - c);
 }
+/**
+ *op_mul - ss
+ *@b: int
+ *@c: int
+ *Return: 0
+ */
 int op_mul(int b, int c)
 {
-	return(b * c);
+	return (b * c);
 }
+/**
+ *op_div - ssd
+ *@b: int
+ *@c: int
+ *Return: 0
+ */
+
 int op_div(int b, int c)
 {
 	return (b / c);
 }
-int op_mod(int b,int c)
+/**
+ *op_mod - sdd
+ *@b: int
+ *@c: int
+ *Return: 0
+ */
+int op_mod(int b, int c)
 {
-	return(b % c);
+	return (b % c);
 }
