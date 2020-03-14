5e8d4f205d360ee0b96c1f6b4332126febf85964
danielj32 <danieljuanito21@hotmail.com>
2020-03-14 14:25:49 +0000

    [C] - added file print number separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ *print_strings - print a string
+ *@separator: ,
+ *@n: integer
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int j;
+	char *pt;
+
+	if (separator == NULL)
+		return;
+
+	va_start(strings, n);
+
+	for (j = 0; j < n; j++)
+	{
+		pt =  va_arg(strings, char *);
+		printf("%s", pt);
+		if (j < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strings);
+}
+
