3efaf9b20790de53041c6128f56a04a0bf9a9050
danielj32 <danieljuanito21@hotmail.com>
2020-03-14 14:28:32 +0000

    [C] - added file print number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - print the numbers
+ *@separator: char pointer
+ *@n: num of elements
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list listl;
+	unsigned int j;
+
+	if (separator == NULL)
+		return;
+
+	va_start(listl, n);
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", va_arg(listl, int));
+		if (j < (n - 1))
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(listl);
+}
