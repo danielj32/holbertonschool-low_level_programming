2eebebde7ce282225664bfdbb801eaf47882b4e0
danielj32 <danieljuanito21@hotmail.com>
2020-03-14 14:16:22 +0000

    [Create] - added file

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+global main
+
+	section .text
+main:	
+	  mov rax, 1        	; write(
+	  mov rdi, 1        	;   STDOUT_FILENO,
+	  mov rsi, msg      	;   "Hello, world!\n",
+	  mov rdx, 17 	;   sizeof("Hello, world!\n")
+	  syscall           	; );
+
+	  mov rax, 60       	; exit(
+	  mov rdi, rdi        	;   EXIT_SUCCESS
+	  syscall           	; );
+
+	section .data
+msg:	 db "Hello, Holberton", 10
