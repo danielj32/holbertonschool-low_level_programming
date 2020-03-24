f722a3a1ec01d1bb28a0836de532bdb9f3de0716
danielj32 <danieljuanito21@hotmail.com>
2020-03-24 15:49:15 +0000

    fixed deleted var deleted count of loop

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,15 +11,12 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *actual = *head;
 	listint_t *next = NULL;
 
-	int ct = 0;
-
 	while (actual != NULL)
 	{
 		next = actual->next;
 		actual->next = before;
 		before = actual;
 		actual = next;
-		ct++;
 	}
 
 	*head = before;
