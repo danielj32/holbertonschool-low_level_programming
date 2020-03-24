7887c3e5fc70366ac1b5d0bb401199b2b10037e6
danielj32 <danieljuanito21@hotmail.com>
2020-03-24 14:57:09 +0000

    new fixed change order in loop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *pt = NULL;
 
-	while (*head && head)
-	{
-		pt = *head;
-		*head = (*head)->next;
-		free(pt);
-	}
+		while (head && *head)
+		{
+			pt = *head;
+			*head = (*head)->next;
+			free(pt);
+		}
 }
