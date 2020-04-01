428b5ad60c86cd5d2489319a5d30bd2504d3842d
danielj32 <danieljuanito21@hotmail.com>
2020-04-01 00:18:00 +0000

    fixed conditional change conditional ||

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,41 +7,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int l1, f2, fr, fw;
-	char buf[1024];
+	int i, j, fir, fiw;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	l1 = open(argv[1], O_RDONLY);
-	f2  = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	i = open(argv[1], O_RDONLY);
+	j  = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 
-	if (f2 == -1)
+	if (j == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
 	do {
-		fr = read(l1, buf, 1024);
-		if (l1 == -1 && fr ==  -1)
+		fir = read(i, buffer, 1024);
+		if (i == -1 || fir ==  -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		fw = write(f2, buf, fr);
+		fiw = write(j, buffer, fir);
 
-		if (fw == -1)
+		if (fiw == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	} while (fr == 1024);
-	if (close(l1) == -1 && close(f2) == -1)
+	} while (fir == 1024);
+	if (close(i) == -1 && close(j) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", l1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
 		exit(100);
 	}
 
