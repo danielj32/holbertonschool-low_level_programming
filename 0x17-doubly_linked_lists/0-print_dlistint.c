34af3098fe518311c6f11d0d5c5b2fa4795a9b73
danielj32 <danieljuanito21@hotmail.com>
2020-05-02 01:19:43 +0000

    added new fix fix in the print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,10 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (temp != NULL)
 	{
-		printf("%d ", temp->n);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		ct++;
 	}
-	printf("\n");
 	return (ct);
 }
